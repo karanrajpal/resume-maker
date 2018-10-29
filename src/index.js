944cc8daa6888cb5c93d0a5b3e3f8de2cd3d6e95
Karan Rajpal <karanrajpal001@gmail.com>
2018-10-29 13:00:49 -0400

    Inital Index

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// Import React
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import Redux
+
+//Import React component
+import App from './js/components/App';
+
+// Import styles
+
+// Import loadState to read from our browser's local storage
+
+// Import our initial state for first time load
+
+// Configuring Redux Store
+
+// const store = configureStore();
+
+// Render App to DOM
+render(
+  <App />,
+  document.getElementById('resume-app')
+);
