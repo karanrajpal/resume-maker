4ddf43e58598e28907483763fd5ece86b91c32ff
Karan Rajpal <karanrajpal001@gmail.com>
2018-11-05 08:19:45 -0500

    Reformatting Section file

--- src/js/components/Section.js
+++ src/js/components/Section.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SectionHeading from './SectionHeading';
 
-const Section = ({ items, primaryColor }) => {
+const Section = ({ sectionData, primaryColor }) => {
+	const {
+		data,
+		title,
+	} = sectionData;
 	return (
-	    <div className='resume-section'>
-	        {
-	        	items.map((item) => (
-		        	<div className='resume-item' key={item.title}>
-		        		<div className='resume-item__title' style={{ 'color': primaryColor }}>{item.title}</div>
-		        		<div className='resume-item__line2'>
-			        		<div className='resume-item__subtitle'>{item.subtitle}</div>
-			        		<div className='resume-item__date'>{item.date}</div>
-			        	</div>
-		        		<div className='resume-item__description'>{item.description}</div>
-		        	</div>
-		        ))
-	        }
-	    </div>
-    );
+		<div className='resume-section'>
+			<SectionHeading heading={title} />
+			{
+				data.map((item) => (
+					<div className='resume-item' key={item.title}>
+						<div className='resume-item__title' style={{ 'color': primaryColor }}>{item.title}</div>
+						<div className='resume-item__line2'>
+							<div className='resume-item__subtitle'>{item.subtitle}</div>
+							<div className='resume-item__date'>{item.date}</div>
+						</div>
+						<div className='resume-item__description' dangerouslySetInnerHTML={{ __html: item.description }}></div>
+					</div>
+				))
+			}
+		</div>
+	);
 };
 
 Section.propTypes = {
-    items: PropTypes.arrayOf(PropTypes.shape({
-    	title: PropTypes.string.isRequired,
-    	subtitle: PropTypes.string.isRequired,
-    	description: PropTypes.string.isRequired,
-    	date: PropTypes.string.isRequired,
-    	link: PropTypes.string,
-    })).isRequired,
+	sectionData: PropTypes.shape({
+		data: PropTypes.arrayOf(PropTypes.shape({
+			title: PropTypes.string.isRequired,
+			subtitle: PropTypes.string.isRequired,
+			description: PropTypes.string.isRequired,
+			date: PropTypes.string.isRequired,
+			link: PropTypes.string,
+		})).isRequired,
+		title: PropTypes.string
+	}),
 };
 
 export default Section;
