4320d9d6ae6e9c5ffbbc1db504e28fc30572194c
Karan Rajpal <karanrajpal001@gmail.com>
2018-11-05 08:19:14 -0500

    Restructuring reducer

--- src/js/state/AppReducer.js
+++ src/js/state/AppReducer.js
@@ -21,50 +21,57 @@ export const initialState = {
             "linkedIn": "linkedin.com",
             "location": "New York"
         },
-        "experience": [
-        {
-            "title": "Moat",
-            "subtitle": "Full Stack Software Developer",
-            "description": "Saving humanity from extinction. My role includes writing test-driven code.",
-            "date": "Feb 2017-Oct 2018"
-        },
-        {
-            "title": "Moat (Intern)",
-            "subtitle": "Full Stack Software Developer",
-            "description": "Thoughtful code that changes the fabric of space and time. Was applauded for my work on improving the efficiency of the process by 30%.",
-            "date": "Jun 2016-Aug 2016"
-        }
-],
-        "projects": [
-        {
-            "title": "Resume Maker",
-            "subtitle": "React, Redux, SASS, ES6",
-            "description": "Wrote the amazing tool that made this resume",
-            "date": "Oct 2018"
-        },
-        {
-            "title": "NLP Bot",
-            "subtitle": "Really smart bot",
-            "description": "The bot in question can answer all your questions. Tread with caution.",
-            "date": "Feb 2018"
-        }
-],
-        "education": [
-        {
-            "title": "New York University",
-            "subtitle": "MS Computer Science, GPA 3.8",
-            "description": "Mastered my ability to entertain crowds.",
-            "date": "Jan 2016-Dec 2016"
-        },
-        {
-            "title": "Another University",
-            "subtitle": "BE Computer Science, GPA 3.9",
-            "description": "Learnt most of my superpowers here. Gained a foundational insight into the ways of the world",
-            "date": "Jan 2010-Aug 2010"
-        }
-],
+        "sections": [
+            {
+                "title": "Professional Experience",
+                "data": [{
+                    "title": "Moat",
+                    "subtitle": "Full Stack Software Developer",
+                    "description": "Saving humanity from extinction. My role includes writing test-driven code.",
+                    "date": "Feb 2017-Oct 2018"
+                },
+                {
+                    "title": "Moat (Intern)",
+                    "subtitle": "Full Stack Software Developer",
+                    "description": "Thoughtful code that changes the fabric of space and time. Was applauded for my work on improving the efficiency of the process by 30%.",
+                    "date": "Jun 2016-Aug 2016"
+                }]
+            },
+            {
+                "title": "Projects",
+                "data": [{
+                    "title": "Resume Maker",
+                    "subtitle": "React, Redux, SASS, ES6",
+                    "description": "Wrote the amazing tool that made this resume",
+                    "date": "Oct 2018",
+                    "link": "http://bit.ly/resume-maker",
+                },
+                {
+                    "title": "NLP Bot",
+                    "subtitle": "Really smart bot",
+                    "description": "The bot in question can answer all your questions. Tread with caution.",
+                    "date": "Feb 2018",
+                    "link": "",
+                }]
+            },
+            {
+                "title": "Education",
+                "data": [{
+                    "title": "New York University",
+                    "subtitle": "MS Computer Science, GPA 3.8",
+                    "description": "Mastered my ability to entertain crowds.",
+                    "date": "Jan 2016-Dec 2016"
+                },
+                {
+                    "title": "Another University",
+                    "subtitle": "BE Computer Science, GPA 3.9",
+                    "description": "Learnt most of my superpowers here. Gained a foundational insight into the ways of the world",
+                    "date": "Jan 2010-Aug 2010"
+                }]
+            }
+        ],
         "skills": ["JavaScript", "Snoozing", "Eating"]
-},
+    },
 };
 
 // Define and export reducer
