e53bfd8d903e9a94e89c1882b0e0e7a123f38c7d
Karan Rajpal <karanrajpal001@gmail.com>
2018-11-19 01:43:42 -0500

    Update margins and rename Section Component

--- src/js/components/Section.js
+++ src/js/components/Section.js
@@ -11,7 +11,7 @@ SectionHeading.propTypes = {
 	heading: PropTypes.string,
 };
 
-const SectionBody = ({ item, primaryColor }) => (
+const SectionItem = ({ item, primaryColor }) => (
 	<div className='resume-item'>
 		<div className='resume-item__line1'>
 			<div className='resume-item__titles'>
@@ -26,17 +26,16 @@ const SectionBody = ({ item, primaryColor }) => (
 	</div>
 );
 
-const SectionBodyCompact = ({ item, primaryColor }) => (
-	<div className='resume-item .resume-item--compact'>
+const SectionItemCompact = ({ item, primaryColor }) => (
+	<div className='resume-item resume-item--compact'>
 		<div className='resume-item__line1'>
 			<div className='resume-item__titles'>
 				<div className='resume-item__title' style={{ 'color': primaryColor }}>{item.title}</div>
 				<div className='resume-item__subtitle'>{item.subtitle}</div>
+				<div className='resume-item__link'><a href={item.link}>{item.link}</a></div>
 			</div>
 			<div className='resume-item__date'>{item.date}</div>
 		</div>
-		{/* <div className='resume-item__line2'>
-		</div> */}
 		<div className='resume-item__description' dangerouslySetInnerHTML={{ __html: item.description }}></div>
 	</div>
 );
@@ -51,11 +50,11 @@ export const Section = ({ sectionData, primaryColor, compact }) => {
 			<SectionHeading heading={title} />
 			{
 				data.map((item) => (
-					<Fragment key={item.title}>
+					<Fragment key={`${item.title}${String(Math.random())}`}>
 						{
 							compact ?
-								<SectionBodyCompact item={item} primaryColor={primaryColor} />
-								: <SectionBody item={item} primaryColor={primaryColor} />
+								<SectionItemCompact item={item} primaryColor={primaryColor} />
+								: <SectionItem item={item} primaryColor={primaryColor} />
 						}
 					</Fragment>
 				))
